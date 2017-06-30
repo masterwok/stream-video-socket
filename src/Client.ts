0a6d2adef45c3b7bb316038d9ddefb4e51881470
Jonathan Trowbridge <jonathan@mapletonhillmedia.com>
2017-06-30 11:19:09 -0600

    master - example.

--- src/Client.ts
+++ src/Client.ts
@@ -1,30 +1,31 @@
 import {Constants} from './Constants';
-import * as render from 'render-media';
 import * as websocketStream from 'websocket-stream';
 import {Duplex} from 'stream';
 import {HexHelper} from './Helpers/HexHelper';
-import * as Multistream from 'multistream';
+import * as render from 'render-media';
 
+const stream: Duplex = websocketStream(Constants.connectionString);
 
-const file = {
-    name: Constants.filePath,
-    createReadStream: function (opts) {
-        const stream: Duplex = websocketStream(Constants.connectionString);
-
-        // stream.on('data', (data: Buffer) => {
-        //     const hex = HexHelper(data);
-        //     console.log(data.length);
-        // });
-
-        return stream;
-    }
-};
-
-render.append(file, 'body', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
+stream.on('data', (data: ArrayBuffer) => {
+    const hex = HexHelper(data);
+    console.log(data.byteLength);
 });
+
+stream.on('finish', (data: ArrayBuffer) => {
+    console.log('Stream finished.');
+});
+
+// const file = {
+//     name: Constants.filePath,
+//     createReadStream: function (opts: any) {
+//     }
+// };
+//
+// render.append(file, 'body', (err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     }
+// });
 
 
 
--- src/Constants.ts
+++ src/Constants.ts
@@ -2,7 +2,8 @@ export class Constants {
     public static readonly host = 'localhost';
     public static readonly port = 3000;
     public static readonly connectionString = `ws://${Constants.host}:${Constants.port}`;
-    public static readonly filePath = '/home/masterwok/Downloads/small.mp4';
+    // public static readonly filePath = '/home/masterwok/Downloads/small.mp4';
+    public static readonly filePath = '/Users/jonathantrowbridge/Downloads/small.mp4';
 
     private constructor() {
     }
